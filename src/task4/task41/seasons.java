7f44abe77c70e7d04b79ba8f33ef047cc9d3b737
PashVal2 <pashkint.valera@mail.ru>
2023-09-29 12:31:07 +0300

    tasks

--- src/task4/task41/seasons.java
+++ src/task4/task41/seasons.java
@@ -1,4 +1,24 @@
 package task4.task41;
 
 public class seasons {
+    public enum Seasons {
+        Spring, Summer, Autumn, Winter
+    }
+
+    public static void printSeasonMessage(Season season) {
+        switch (season) {
+            case Summer:
+                System.out.println("Я люблю лето");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Создаем переменную для хранения любимого времени года
+        Season favoriteSeason = Season.SUMMER;
+        System.out.println("Лето - это отличное время года для отдыха и путешествий!");
+
+        printSeasonMessage(favoriteSeason);
+    }
 }
+
